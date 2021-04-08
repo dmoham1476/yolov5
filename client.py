5d87d852d863bb76375213f4dc3f0db63669343c
root <mohdeepa@gmail.com>
2021-04-08 06:07:06 +0000

    client changes

---
+++ client.py
@@ -0,0 +1,23 @@
+import requests
+import json
+import cv2
+import os
+import base64
+
+json_file = 'api_data.json'
+url = 'http://localhost:5000/detect'
+with open(json_file) as f:
+    data = json.load(f)
+    print(data)
+
+image_dir = '/home/deepa/work/yolov5/data/images'
+img_list = []
+for file in os.listdir(image_dir):
+    filepath = os.path.join(image_dir, file)
+    image = cv2.imread(filepath)
+    string = base64.b64encode(cv2.imencode('.jpg', image)[1]).decode()
+    img_list.append(string)
+data["numpy_list"] = img_list
+
+server_return = requests.post(url, json=data)
+print(server_return.text)
