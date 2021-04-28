216cf7d423a0735baeaeb7089fb0e3bbe47a8ce2
Deepa Mohan <deepa.mohan@walmartlabs.com>
2021-04-28 13:12:33 -0700

    realsense flask

--- client_realsense.py
+++ client_realsense.py
@@ -10,4 +10,11 @@ with open(json_file) as f:
     data = json.load(f)
 
 server_return = requests.post(url, json=data)
-print(server_return)
+print(server_return[1])
+jpg_original = base64.b64decode(server_return["undetected_item"])
+jpg_as_np = np.frombuffer(jpg_original, dtype=np.uint8)
+
+cv2.imwrite('color_img.jpg', jpg_as_np)
+cv2.imshow('Color image', jpg_as_np)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
