a098148b7c6e7a93d7baaa68bb5f573d7cdb0f0e
Deepa Mohan <deepa.mohan@walmartlabs.com>
2021-04-28 12:11:38 -0700

    realsense flask

--- rs_render_undetected.py
+++ rs_render_undetected.py
@@ -192,7 +192,7 @@ def detect(save_img=False):
             bg_removed = np.where((depth_image_3d > clipping_distance) | (depth_image_3d <= 0), grey_color, covered_img)
 
             print("testing.........", type(bg_removed))
-            return jsonfiy(bg_removed)
+            return jsonify(bg_removed)
             #cv2.namedWindow("covered_img", cv2.WINDOW_NORMAL)
             #cv2.imshow("covered_img", covered_img)
             #cv2.namedWindow('RealSense', cv2.WINDOW_NORMAL)
