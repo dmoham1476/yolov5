97ed1971ce71a4005db586d50b564467e74a0e7b
Deepa Mohan <deepa.mohan@walmartlabs.com>
2021-04-28 13:23:57 -0700

    realsense flask

--- client_realsense.py
+++ client_realsense.py
@@ -3,6 +3,7 @@ import json
 import cv2
 import os
 import base64
+import numpy as np
 
 json_file = 'api_data_camera.json'
 url = 'http://localhost:8000/detect'
@@ -10,6 +11,7 @@ with open(json_file) as f:
     data = json.load(f)
 
 server_return = requests.post(url, json=data)
+server_return = server_return.json()
 print(server_return["webcam.jpg"])
 jpg_original = base64.b64decode(server_return["undetected_item"])
 jpg_as_np = np.frombuffer(jpg_original, dtype=np.uint8)
